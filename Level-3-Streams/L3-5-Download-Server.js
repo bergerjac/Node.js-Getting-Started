4ef8adcaed6f07e11f81807ecfb5e5f7f3bac550
Jake Berger <jberger.public@live.com>
2014-05-05 20:18:35 +0200

    L3-5-Download-Server.js init;

--- Level-3-Streams/L3-5-Download-Server.js
+++ Level-3-Streams/L3-5-Download-Server.js
@@ -5,3 +5,18 @@
  - - allows you to download the icon.png file.
  - - use 'pipe' to send the read stream into the response
  */
+
+var fs = require('fs');
+var http = require('http');
+
+http.createServer(
+    function(request, response)
+    {
+        response.writeHead(
+            200,
+            {'Content-Type': 'image/png'}
+        );
+
+        var file = fs.createReadStream('icon.png');
+    }
+);
