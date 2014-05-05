4a19f091f228447ab7b8cea8dd1491a1166e0c25
Jake Berger <jberger.public@live.com>
2014-05-05 22:58:33 +0200

    Level-5-Express/L5-1-Express-Routes.js init;

---
+++ Level-5-Express/L5-1-Express-Routes.js
@@ -0,0 +1,11 @@
+/*
+ Express routes
+
+ - Create an express route
+ - - responds to GET requests at the URL /tweets
+ - - responds with the file tweets.html located in the same directory as app.js
+ Make sure you create the app and listen on port 8080
+ */
+
+var express = require('express');
+var app = express.createServer();
