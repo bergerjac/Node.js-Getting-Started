74fa87b96ac45f2a9998af8fb35d619eece37e37
Jake Berger <jberger.public@live.com>
2014-05-05 23:01:06 +0200

    L5-1-Express-Routes.js success;

--- Level-5-Express/L5-1-Express-Routes.js
+++ Level-5-Express/L5-1-Express-Routes.js
@@ -9,3 +9,12 @@
 
 var express = require('express');
 var app = express.createServer();
+
+app.get(
+    '/tweets', function(request, response)
+    {
+        response.sendfile(__dirname + "/tweets.html");
+    }
+);
+
+app.listen(8080);
