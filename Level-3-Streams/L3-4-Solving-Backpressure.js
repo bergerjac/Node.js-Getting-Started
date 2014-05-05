99e38d24a1d9ff25739083e12e484cb5eee0260e
Jake Berger <jberger.public@live.com>
2014-05-05 20:14:56 +0200

    L3-4 complete;

--- Level-3-Streams/L3-4-Solving-Backpressure.js
+++ Level-3-Streams/L3-4-Solving-Backpressure.js
@@ -15,7 +15,15 @@ var newFile = fs.createWriteStream("icon-new.png");
 
 file.on('data', function(chunk)
 {
-    newFile.write(chunk);
+    var buffer_good = newFile.write(chunk);
+    if (!buffer_good)
+    {
+        file.pause();
+    }
+});
+newFile.on("drain", function()
+{
+    file.resume();
 });
 
 file.on('end', function()
