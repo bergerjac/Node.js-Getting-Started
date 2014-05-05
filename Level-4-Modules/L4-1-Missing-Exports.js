354d079b0ddd0cc4896acfd40d320a1d0c59d5af
Jake Berger <jberger.public@live.com>
2014-05-05 22:17:29 +0200

    L4-1-Missing-Exports.js init;

---
+++ Level-4-Modules/L4-1-Missing-Exports.js
@@ -0,0 +1,13 @@
+/*
+ Missing exports
+
+ Notice the two different files below: app.js on the right side and high_five.js on the left.
+ The code as it's written will not work, high_five isn't exporting anything.
+ Add the proper exports line to have a successful high five!
+ * */
+
+// high_five.js
+var highfive = function()
+{
+    console.log("<smack!!>");
+};
