90b5644cdd28e4a52d7b00af662ff06b69c53a89
Jake Berger <jberger.public@live.com>
2014-05-06 11:58:24 +0200

    L7-3-Working-with-Lists-I.js success;

--- Level-7-Persisting-Data/L7-3-Working-with-Lists-I.js
+++ Level-7-Persisting-Data/L7-3-Working-with-Lists-I.js
@@ -11,3 +11,12 @@ var client = redis.createClient();
 
 var question1 = "Where is the dog?";
 var question2 = "Where is the cat?";
+
+client.lpush('questions', question1, function(error, reply)
+{
+
+});
+client.lpush('questions', question2, function(error, reply)
+{
+
+});
