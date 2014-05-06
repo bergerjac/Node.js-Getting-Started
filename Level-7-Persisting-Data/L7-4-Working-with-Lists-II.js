f47edb16ac5ab847374a8165f2bd8bab309922c4
Jake Berger <jberger.public@live.com>
2014-05-06 12:03:33 +0200

    L7-4-Working-with-Lists-II.js success;

--- Level-7-Persisting-Data/L7-4-Working-with-Lists-II.js
+++ Level-7-Persisting-Data/L7-4-Working-with-Lists-II.js
@@ -1,8 +1,19 @@
 /*
  Working with Lists II
 
- Now that we have seeded the questions list, use the lrange command to return all of the items and log them.
+ use the lrange command to return all of the items
+ log them.
  */
 
 var redis = require('redis');
 var client = redis.createClient();
+
+client.lrange('questions', 0, -1, function(error, questions)
+{
+    questions.forEach(
+        function(question)
+        {
+            console.log(question);
+        }
+    );
+});
