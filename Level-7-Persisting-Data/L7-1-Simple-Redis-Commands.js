b1c1e8370856c4519e442b4e6ec3e42411abd88d
Jake Berger <jberger.public@live.com>
2014-05-06 11:46:42 +0200

    init L7-1-Simple-Redis-Commands.js;

---
+++ Level-7-Persisting-Data/L7-1-Simple-Redis-Commands.js
@@ -0,0 +1,10 @@
+/*
+ Simple Redis Commands
+
+ Let's start practicing using the redis key-value store from our node application.
+
+ - require the redis module
+ - create a redis client that we can use to call commands on our redis server.
+ - use the client to set the name key to your name.
+
+ */
