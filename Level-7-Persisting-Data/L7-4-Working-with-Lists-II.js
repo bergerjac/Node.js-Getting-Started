97ed2e2dfc15d4cc6c40b1bb1984f8d0fd31ed57
Jake Berger <jberger.public@live.com>
2014-05-06 11:59:26 +0200

    init L7-4-Working-with-Lists-II.js;

---
+++ Level-7-Persisting-Data/L7-4-Working-with-Lists-II.js
@@ -0,0 +1,8 @@
+/*
+ Working with Lists II
+
+ Now that we have seeded the questions list, use the lrange command to return all of the items and log them.
+ */
+
+var redis = require('redis');
+var client = redis.createClient();
