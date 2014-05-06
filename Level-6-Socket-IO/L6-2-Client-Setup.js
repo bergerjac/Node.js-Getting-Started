845c4c6847eb7592361bfb1def0e818158a45b82
Jake Berger <jberger.public@live.com>
2014-05-06 10:57:04 +0200

    L6-2-Client-Setup.js init;

---
+++ Level-6-Socket-IO/L6-2-Client-Setup.js
@@ -0,0 +1,9 @@
+/*
+Client socket.io setup
+
+html file
+- load socket.io.js script /socket.io/socket.io.js
+- - use io.connect to connect to socket.io on the server at http://localhost:8080
+
+Express knows to serve the socket.io client js for this path.
+*/
